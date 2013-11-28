07061e9db14f9f5d7ad78eb96256c2e7658e3b9f
inzl <zliobaite@gmail.com>
2013-11-28 23:51:42 +0200

    dataset csv and more code

--- prepareDataJUFO.py
+++ prepareDataJUFO.py
@@ -1,8 +1,11 @@
 import csv as csv
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from time import time
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, mean_absolute_error
 import numpy as np
+from stemming.porter2 import stem
+import re
+from random import shuffle
 
 def extractNamesJUFO(fileName):
     csvFileObject = csv.reader(open(fileName, 'rU'),delimiter=";")
@@ -14,15 +17,53 @@ def extractNamesJUFO(fileName):
     for row in csvFileObject:
         rankings.append(row[hi2])
         titles.append(row[hi])
-    #print(rankings[5])
-    #print(titles[5])
     return titles,rankings
 
+def writeDataset(titles,labels):
+    cwriter = csv.writer(open('JUFOdataset.csv', 'w'),delimiter=';')
+    cwriter.writerow(["TITLE","RATING"])
+    for sk in range(len(labels)):
+        newLine = [titles[sk],labels[sk]]
+        cwriter.writerow(newLine)
+
+def extractNAmesPredatory(fileName):
+    f = open(fileName)
+    titles = f.readlines()
+    f.close()
+    rankings = ['-1'] * len(titles)
+    return titles, rankings
+
 def makeVectorsTrain(corpus):
-    vectorizer = CountVectorizer(min_df=0.0006)
+    #corpus = cleanCorpus(corpus)
+    #corpus = stemCorpus(corpus)
+    vectorizer = CountVectorizer(max_features = 1000,strip_accents='unicode')
+    #vectorizer = CountVectorizer(strip_accents='unicode')
     corpus = [cp.decode('latin-1').encode('utf-8') for cp in corpus]
     X = vectorizer.fit_transform(corpus)
     return X, vectorizer
+
+def numerLabels(y):
+    sk = 0
+    for tt in y:
+        if tt =='-':
+            y[sk] = '0'
+        sk = sk + 1
+    y = [int(tt) for tt in y]
+    return y
+
+def roundPredictions(y):
+    y = [int(round(tt,0)) for tt in y]
+    return y
+
+
+def mae(ytest, ypred):
+    ypred = numerLabels(ypred)
+    ytest = numerLabels(ytest)
+    #print(ypred)
+    mae = mean_absolute_error(ytest,ypred)
+    #randmae = mean_absolute_error(ytest,np.random.permutation(ytest))
+    randnorm = mae/0.58
+    return mae, randnorm
 
 def makeVectorsTest(corpus,vectorizer):
     corpus = [cp.decode('latin-1').encode('utf-8') for cp in corpus]
@@ -31,7 +72,40 @@ def makeVectorsTest(corpus,vectorizer):
 
 def kappa(ytrue,ypred):
     acc = accuracy_score(ytrue, ypred)
-    ypred = np.random.permutation(ypred)
-    acc0 = accuracy_score(ytrue, ypred)
+    cm = confusion_matrix(ytrue, ypred)
+    priorPredictions = sum(cm)
+    priorTrue = sum(cm.T)
+    N = sum(priorTrue)
+    priorTrue = 1.0*priorTrue/N
+    print('priors',priorTrue)
+    priorPredictions = 1.0*priorPredictions/N
+    acc0 = sum(priorPredictions * priorTrue)
     kappa = (acc - acc0)/(1 - acc0)
     return kappa
+
+def stemCorpus(corpus):
+    #print(corpus[1])
+    sk = 0
+    #print(corpus)
+    for row in corpus:
+        #print(row)
+        row = [stem(word) for word in row.split()]
+        #print(row)
+        corpus[sk] = ' '.join(row)
+        sk = sk + 1
+    #print(corpus[1])
+    print('done stemming')
+    return corpus
+
+def cleanCorpus(corpus):
+    sk = 0
+    #print(corpus)
+    for row in corpus:
+        #print(row)
+        row = [re.sub('^[^a-zA-z]*|[^a-zA-Z]*$','',word) for word in row.lower().replace('-',' ').split()]
+        #print(row)
+        corpus[sk] = ' '.join(row)
+        sk = sk + 1
+    #print(corpus[1])
+    print('done cleaning')
+    return corpus
--- runAnalyze.py
+++ runAnalyze.py
@@ -10,75 +10,94 @@ from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.lda import LDA
 from sklearn.qda import QDA
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 import numpy as np
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, classification_report, mean_absolute_error
 from scipy.sparse import hstack
+from sklearn.feature_selection import SelectKBest,chi2
+from sklearn import linear_model, svm
 
 #parameter setting
 file = 'lehdetjasarjat_13112013.csv'
 #file = 'leh_small_example.csv'
-
+file2 = 'predatory_journals.txt'
 
 #operation
 titles,labels = prepareDataJUFO.extractNamesJUFO(file)
-#print(titles)
+
+#predatory journals
+titles2,labels2 = prepareDataJUFO.extractNAmesPredatory(file2)
+titles = titles + titles2
+labels = labels + labels2
+labels = prepareDataJUFO.numerLabels(labels)
+
+#prepareDataJUFO.writeDataset(titles,labels)
+
 print('number of journals',len(titles))
 
+#extra features
 Xtrain,Xtest,ytrain,ytest = train_test_split(titles,labels,test_size=0.5,random_state=42)
-#print(Xtrain)
 Wtrain = [ len(x.split()) for x in Xtrain]
 Wtest = [ len(x.split()) for x in Xtest]
-#Ltrain = [ len(x) for x in Xtrain]
-#Ltest = [ len(x) for x in Xtest]
-
-#print(Wtest)
+Ltrain = [ len(x) for x in Xtrain]
+Ltest = [ len(x) for x in Xtest]
+ALtrain = [ np.mean( len(x.split()) ) for x in Xtrain]
+ALtest = [ np.mean( len(x.split()) ) for x in Xtest]
 
 Xtrain, transformation = prepareDataJUFO.makeVectorsTrain(Xtrain)
-#print(Xtrain)
 print('words ',len(transformation.get_feature_names()))
 Xtest = prepareDataJUFO.makeVectorsTest(Xtest,transformation)
 
+#add extra features
 Xtrain = hstack((Xtrain, np.matrix(Wtrain).T))
 Xtest = hstack((Xtest, np.matrix(Wtest).T))
+Xtrain = hstack((Xtrain, np.matrix(Ltrain).T))
+Xtest = hstack((Xtest, np.matrix(Ltest).T))
+Xtrain = hstack((Xtrain, np.matrix(ALtrain).T))
+Xtest = hstack((Xtest, np.matrix(ALtest).T))
 
-#Xtrain = hstack((Xtrain, np.matrix(Ltrain).T)) - makes things worse
-#Xtest = hstack((Xtest, np.matrix(Ltest).T))
+featureSelector = SelectKBest(chi2, 300)
+Xtrain = featureSelector.fit_transform(Xtrain, ytrain)
+Xtest = featureSelector.transform(Xtest)
+#print(featureSelector.get_support())
 
-#Xtrain = np.matrix(Wtrain).T
-#Xtest = np.matrix(Wtest).T
 
 results = []
 classifiers = [KNeighborsClassifier(3),
-               KNeighborsClassifier(5),
-               KNeighborsClassifier(7),
-               #SVC(kernel="linear", C=0.025),
-               #SVC(gamma=2, C=1),
-               #DecisionTreeClassifier(max_depth=5),
-               #RandomForestClassifier(max_depth=20, n_estimators=10),
-               #AdaBoostClassifier(),
-               #GaussianNB(),
-               #LDA(),
-               #QDA(),
-               MultinomialNB()]
-names = ["KNN(3)","KNN(5)","KNN(7)","MNB"]
+               MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True),
+               BernoulliNB(alpha=1.0, binarize=0.0, class_prior=None, fit_prior=True)]
+names = ["KNN(3)","MNB","BernNB"]
 i=0
 for clf in classifiers:
-    #clf = MultinomialNB()
-    #MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
     clf.fit(Xtrain, ytrain)
     ypred = clf.predict(Xtest)
-    #print(ypred)
     acc = accuracy_score(ytest, ypred)
-    #print('Accuracy ',acc)
+    mae, rmae = prepareDataJUFO.mae(ytest, ypred)
     kp = prepareDataJUFO.kappa(ytest, ypred)
-    #print('Kappa ',kp)
-    print(names[i],'Acc ',acc,' kappa ',kp)
+    print(names[i],'Acc ',acc,' kappa ',kp, ' mae ',mae, ' rand normalized ',rmae)
     results.append([acc,kp])
     i=i+1
+    #cm = confusion_matrix(ytest, ypred,labels=['-1','-','1','2','3'])
+    #print(cm)
+    print classification_report(ytest, ypred)
 
-print('<><><><>')
-cm = confusion_matrix(ytest, ypred,labels=['-','1','2','3'])
-print(cm)
-#print(results)
+probabilities = clf.feature_log_prob_
+probabilities = np.exp(probabilities)
+colSums = probabilities.sum(axis=0)
+probabilities = probabilities / colSums[np.newaxis,:]
+print(probabilities)
+
+#analysis of selected features
+indSorted   = np.argsort(featureSelector.scores_)[::-1]
+print(indSorted[0:30])
+labelsList = list(transformation.vocabulary_) + list(["#words"]) + list(["#symbols"]) + list(["av.symb.per.word"])
+for sk in range(20):
+    indNow = indSorted[sk]
+    out = "\t"+labelsList[indNow]+"\t\t"+str(np.round(100*probabilities[0,sk])).strip('.0')+"%\t\t"+str(np.round(100*probabilities[1,sk])).strip('.0')+"%\t\t"+str(np.round(100*probabilities[2,sk])).strip('.0')+"%\t\t"+str(np.round(100*probabilities[3,sk])).strip('.0')+"%\t\t"+str(np.round(100*probabilities[4,sk])).strip('.0')+"%"
+    print(out)
+
+clf.fit(Xtrain, prepareDataJUFO.numerLabels(ytrain))
+ypred = prepareDataJUFO.roundPredictions(clf.predict(Xtest))
+mae = mean_absolute_error(prepareDataJUFO.numerLabels(ytest),prepareDataJUFO.numerLabels(ypred))
+print('regression mae',mae)
